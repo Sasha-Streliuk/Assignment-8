afb0ae6c3711870bc1930815d655397b4e87f717
Sasha-Streliuk <alexstreliuk@gmail.com>
2022-12-25 18:50:40 +0200

    class Paddle

--- main.py
+++ main.py
@@ -4,12 +4,30 @@ WIDTH = 600
 HEIGHT = 800
 
 
+class Paddle:
+
+    def __init__(self):
+        self.actor = Actor('paddle.png', center=(WIDTH // 2, HEIGHT - 50))
+
+    def draw(self):
+        self.actor.draw()
+
+
+paddle = Paddle()
+
+
 def draw():
-    pass
+    screen.clear()
+    paddle.draw()
 
 
 def update(dt):
     pass
+
+
+def on_mouse_move(pos):
+    x, y = pos
+    paddle.actor.x = x
 
 
 pgzrun.go()
